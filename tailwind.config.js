af8e1637eff45fa77015fed54cd8c4674e115ce8
mianxiu <mianxiu@users.noreply.github.com>
2021-02-09 16:36:16 +0800

    init tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+    width:{
+      'icon-18':'18px',
+      'icon-24':'24px',
+      'icon-30':'30px',
+    }
+    },
+    height:{
+      'px-24':'24px',
+      'px-30':'30px',
+    }
   },
   variants: {
     extend: {},
