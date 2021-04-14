860061497ef2202be0e933a896d3d3a96f32fa42
mianxiu <mianxiu@users.noreply.github.com>
2021-04-14 19:42:07 +0800

    Update initPanel.js

--- plugin/action/initPanel.js
+++ plugin/action/initPanel.js
@@ -193,6 +193,7 @@ function upgradeMain() {
                 document.querySelector(uxpPanel).setAttribute("panel", panelMode.compressExport);
                 upgradeNav();
                 upGradePanel();
+                //compressAndExport.fuck();
                 compressAndExport.mergeMainToSmartObject();
             };
             dupliceVectorId = "#duplice-vector";
